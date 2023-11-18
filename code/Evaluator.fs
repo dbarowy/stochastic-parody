dba9d1a7df908f71fffdf16b68cdf5befdfa28f6
Matt Laws <mdl4@williams.edu>
2023-11-18 18:27:16 -0500

    q-refactor

--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -1,6 +1,6 @@
 module Evaluator
+open Parser
 open System.IO
-open Combinator
 
 type Entry = { word: string; emph: string; rhyme: string List}
 
@@ -19,6 +19,80 @@ let readDict =
             let myEmph = spl[2..] |> List.map (fun s -> if s.Contains("2") then "2" else (if s.Contains("1") then "1" else (if s.Contains("0") then "0" else ""))) |> (String.concat "")
             {word = spl[0]; emph = myEmph; rhyme = myRhyme} :: (helper xs)
     
-    let x = helper (dictList[0..100000] |> Array.toList)
-    // printfn "%A" x[0..5]
-    0
+    let x = helper (dictList[0..10000] |> Array.toList)
+    // printfn "%A" x[10000]
+    x
+
+let evalSentiment sent = 
+    if sent = "happy" then ["adore"; "apple"; "atop"] else []
+
+
+let evalKeywords kws = 
+    kws
+
+let rec searchWord word (cmuDict: Entry List) = 
+    match cmuDict with
+    | x::xs when x.word = word -> x
+    | [] -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
+    | x::xs -> searchWord word xs
+
+
+let rec searchEmph emph (word: string) (cmuDict: Entry List) = 
+    match cmuDict with
+    | x::xs when (x.emph = emph) && (x.word <> word) -> x
+    | [] -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
+    | x::xs -> searchEmph emph word xs
+let convert lines cmuDict =
+    let rec helpConvert ls = 
+        match ls with 
+        | [] -> []
+        | x::xs -> 
+            let myWord: Entry = searchWord x cmuDict
+            let myEmph = myWord.emph
+            let newWord = (searchEmph myEmph x cmuDict).word
+            newWord::(helpConvert xs)
+    helpConvert lines
+
+let evalProg (ps: Expr list) = 
+    let cmuDict = readDict
+    let wordList = 
+        (match ps[0] with
+        | Sentiment x -> evalSentiment x
+        | Keywords x -> evalKeywords x
+        | Line x -> []) @
+        match ps[1] with
+        | Sentiment x -> evalSentiment x
+        | Keywords x -> evalKeywords x
+        | Line x -> []
+
+        
+
+    let rec helper (ls: Expr list) = 
+        match ls with
+        | [] -> []
+        | x::xs ->
+            match x with
+            | Sentiment y -> 
+                // helper (xs, (words @ (evalSentiment y)))
+                helper xs
+
+                // helper xs
+            | Keywords y -> 
+                // helper (xs, (words @ ((evalKeywords y))))
+                helper xs
+
+                // wordList := (evalKeywords x) @ wordList
+                // helper xs
+            | Line x ->
+
+                (convert x cmuDict) :: helper xs
+                // "TODO" :: helper xs
+
+    // printfn "%A" (wordList)
+    // (helper ps[2..])
+    convert ["apple"; "boston"] [{word = "apple"; emph = "01"; rhyme = ["FAIL"]}; {word = "boston"; emph = "01"; rhyme = ["FAIL"]}]
+
+        
+
+                
+            
--- code/Parser.fs
+++ code/Parser.fs
@@ -3,7 +3,7 @@ open Combinator
 
 type Expr =
 | Sentiment of string
-| Keyword of string List
+| Keywords of string List
 | Line of string List
 // TODO think about songs with numbers
 
@@ -12,11 +12,11 @@ let pneed a b c = (pseq a b c) <|> (pseq b a c)
 let pword = (pmany1 pletter) |>> stringify
 let pline = pseq (pmany0 (pleft (pword) (pchar ' '))) (pleft pword pnl) (fun (f, e) -> Line (f @ [e]))
 
-let pkeyword = pleft (pright (pstr "<KEYWORDS> ") (pmany0 pword) |>> (fun x -> Keyword x)) pnl
+let pkeywords = pleft (pright (pstr "<KEYWORDS> ") (pmany0 pword) |>> (fun x -> Keywords x)) pnl
 
 let psentiment = pleft (pright (pstr "<SENTIMENT> ") (pword) |>> (fun x -> Sentiment x)) pnl
 
-let pexpr = pseq (pseq psentiment pkeyword (fun (a, b) -> a::[b])) (pmany1 pline) (fun (a, b) -> a @ b)
+let pexpr = pseq (pseq psentiment pkeywords (fun (a, b) -> a::[b])) (pmany1 pline) (fun (a, b) -> a @ b)
 
 // let pexpr = pmany1 pline
 let grammar = pleft pexpr peof
