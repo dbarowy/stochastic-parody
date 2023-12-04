184f3bf76a571db15ca5926bda352ba537ea5b9b
Matt Laws <mdl4@williams.edu>
2023-12-04 16:18:12 -0500

    updated eval

--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -4,19 +4,26 @@ open System.IO
 
 type Entry = { word: string; emph: string; rhyme: string List}
 
-
+// run checker for if words are in dictionary before running evaluator
 let readDict =
     let dict = File.ReadAllText "cmu_dict"
     let dictArr = dict.Split('\n')
-    let formatArr: Entry array = Array.create (Array.length dictArr) {word = ""; emph = ""; rhyme = [""]}
+    // let formatArr: Entry array = Array.create (Array.length dictArr) {word = ""; emph = ""; rhyme = [""]}
     // TODO remove for loop
-    for i in 0.. (Array.length dictArr) - 1 do
+
+
+    let inds = [|0.. (Array.length dictArr) - 1|]
+
+    let formatArray = inds |> Array.map (fun i -> 
         let spl: string list = dictArr[i].Split(' ') |> Array.toList
         let len = (List.length spl)
         let myRhyme = if len <= 3 then [spl[len - 1]] else [spl[len - 2] ; spl[len - 1]]
         let myEmph = spl[2..] |> List.map (fun s -> if s.Contains("2") then "2" else (if s.Contains("1") then "1" else (if s.Contains("0") then "0" else ""))) |> (String.concat "")
-        formatArr[i] <- {word = spl[0]; emph = myEmph; rhyme = myRhyme}
-    formatArr
+        {word = spl[0]; emph = myEmph; rhyme = myRhyme}
+        )
+    // for i in 0.. (Array.length dictArr) - 1 do
+        
+    formatArray
 
 let evalSentiment sent = 
     if sent = "happy" then ["adore"; "apple"; "atop"] else []
@@ -28,16 +35,18 @@ let evalKeywords kws =
 let rec searchWord n (word: string) (cmuDict: Entry array) = 
 
     match n with
-    | 0 -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
+    | -1 -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
     | i -> 
         if (cmuDict[i].word = word.ToUpper()) then cmuDict[i] else searchWord (n - 1)  word cmuDict 
 
 // TODO make this be a mod circle
 let rec searchEmph emph n (word: string) (cmuDict: Entry array) = 
     match n with
-    | 0 -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
+    | -1 -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
     | i -> if cmuDict[i].emph = emph && cmuDict[i].word <> word.ToUpper() then cmuDict[i] else searchEmph emph (n - 1) word cmuDict 
 
+// TODO: add common words
+// TODO: basically we take 
 let convert (lines: string list) cmuDict =
     let len = ((Array.length cmuDict) - 1)
 
