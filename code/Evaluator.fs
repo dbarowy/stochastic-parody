fe4b7d4c8b450c748fbc8b0fc975e91196e3c87e
Matt Laws <mdl4@williams.edu>
2023-12-06 17:52:37 -0500

    hashmap + common dict implemented

--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -2,15 +2,28 @@ module Evaluator
 open Parser
 open System.IO
 
+open System.Collections
+
 type Entry = { word: string; emph: string; rhyme: string List}
+type features = { emph: string; rhyme: string List}
+
 
 // run checker for if words are in dictionary before running evaluator
 let readDict =
-    let dict = File.ReadAllText "cmu_dict"
-    let dictArr = dict.Split('\n')
-    // let formatArr: Entry array = Array.create (Array.length dictArr) {word = ""; emph = ""; rhyme = [""]}
-    // TODO remove for loop
+    let wordToFeature = new Hashtable();
+    let emphToWord = new Hashtable();
+    let commonEmphToWord = new Hashtable();
 
+
+    let dict = File.ReadAllText "cmu_dict"
+    let common = File.ReadAllText "common.txt"
+
+    let dictArr = dict.Split('\n')
+    let commonArr = common.Split('\n')
+
+    // let cinds = [|0.. (Array.length commonArr) - 1|]
+    
+    // let formatArr: Entry array = Array.create (Array.length dictArr) {word = ""; emph = ""; rhyme = [""]}
 
     let inds = [|0.. (Array.length dictArr) - 1|]
 
@@ -19,11 +32,36 @@ let readDict =
         let len = (List.length spl)
         let myRhyme = if len <= 3 then [spl[len - 1]] else [spl[len - 2] ; spl[len - 1]]
         let myEmph = spl[2..] |> List.map (fun s -> if s.Contains("2") then "2" else (if s.Contains("1") then "1" else (if s.Contains("0") then "0" else ""))) |> (String.concat "")
-        {word = spl[0]; emph = myEmph; rhyme = myRhyme}
+        wordToFeature.Add(spl[0], box {emph = myEmph; rhyme = myRhyme})
+        if emphToWord.ContainsKey(myEmph) then
+            //  (emphToWord[myEmph] = spl[0]::(emphToWord[myEmph]))
+           let old: string list= emphToWord[myEmph] |> unbox
+           emphToWord.Remove(myEmph)
+           emphToWord.Add(myEmph, box (spl[0]::old))
+
+        else
+            emphToWord.Add(myEmph, box [spl[0]])
+        
         )
     // for i in 0.. (Array.length dictArr) - 1 do
+
+    let dummy = commonArr |> Array.map (fun i ->
+        let feat: features = wordToFeature[(i.ToUpper())] |> unbox
+        let myEmph = feat.emph
+        if commonEmphToWord.ContainsKey(myEmph) then
+            //  (emphToWord[myEmph] = spl[0]::(emphToWord[myEmph]))
+           let old: string list = commonEmphToWord[myEmph] |> unbox
+           commonEmphToWord.Remove(myEmph)
+           commonEmphToWord.Add(myEmph, box (i::old))
+
+        else
+            commonEmphToWord.Add(myEmph, box [i])
         
-    formatArray
+        // commonEmphToWord.Add(i, emphToWord[i])
+    
+        )
+        
+    (wordToFeature, emphToWord, commonEmphToWord)
 
 let evalSentiment sent = 
     if sent = "happy" then ["adore"; "apple"; "atop"] else []
@@ -32,38 +70,45 @@ let evalSentiment sent =
 let evalKeywords kws = 
     kws
 
-let rec searchWord n (word: string) (cmuDict: Entry array) = 
+// let rec searchWord n (word: string) (cmuDict: Entry array) = 
 
-    match n with
-    | -1 -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
-    | i -> 
-        if (cmuDict[i].word = word.ToUpper()) then cmuDict[i] else searchWord (n - 1)  word cmuDict 
+//     match n with
+//     | -1 -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
+//     | i -> 
+//         if (cmuDict[i].word = word.ToUpper()) then cmuDict[i] else searchWord (n - 1)  word cmuDict 
 
-// TODO make this be a mod circle
-let rec searchEmph emph n (word: string) (cmuDict: Entry array) = 
-    match n with
-    | -1 -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
-    | i -> if cmuDict[i].emph = emph && cmuDict[i].word <> word.ToUpper() then cmuDict[i] else searchEmph emph (n - 1) word cmuDict 
+// // TODO make this be a mod circle
+// let rec searchEmph emph n (word: string) (cmuDict: Entry array) = 
+//     match n with
+//     | -1 -> {word = "FAIL"; emph = "FAIL"; rhyme = ["FAIL"]}
+//     | i -> if cmuDict[i].emph = emph && cmuDict[i].word <> word.ToUpper() then cmuDict[i] else searchEmph emph (n - 1) word cmuDict 
 
 // TODO: add common words
 // TODO: basically we take 
-let convert (lines: string list) cmuDict =
-    let len = ((Array.length cmuDict) - 1)
+let convert (lines: string list) (wtf: Hashtable) (etw: Hashtable) (cetw: Hashtable) =
+    // let len = ((Array.length cmuDict) - 1)
 
     let rnd = System.Random()
     let rec helpConvert ls = 
-        match ls with 
+        match (ls: string list) with 
         | [] -> []
         | x::xs -> 
-            let myWord: Entry = searchWord (len) x cmuDict // used to be len
-            let myEmph = myWord.emph
-            let newWord = (searchEmph myEmph ((rnd.Next() % len)) x cmuDict).word // (rnd.Next() % len)
+            let myWord: features = wtf[(x.ToUpper())] |> unbox // used to be len
+            let (myEmph: string) = myWord.emph
+            // printfn "%A" myWord
+            let translator = if cetw.ContainsKey(myEmph) then cetw else etw
+            let wordList: string list = (translator[myEmph]) |> unbox
+            let len = wordList.Length
+            // printfn "%A" (etw.ContainsKey(myEmph))
+            // printfn "%A" (wordList[0])
+            let newWord = wordList[rnd.Next() % (len - 1)] //rnd.Next() % len
+
             newWord::(helpConvert xs)
     helpConvert lines
 
 let evalProg (ps: Expr list) = 
     // TODO combine
-    let cmuDict = readDict
+    let wtf, etw, cetw = readDict
     let wordList = 
         (match ps[0] with
         | Sentiment x -> evalSentiment x
@@ -87,7 +132,7 @@ let evalProg (ps: Expr list) =
                 helper xs
 
             | Line x ->
-                (convert x cmuDict) :: helper xs
+                (convert x wtf etw cetw) :: helper xs
 
 
     (helper ps[2..])
--- code/Program.fs
+++ code/Program.fs
@@ -6,14 +6,17 @@ open System.IO
 [<EntryPoint>]
 let main args =
 
-    try
-        let file = args.[0]
-        let input = File.ReadAllText file
-        let astOp = parse input
-        match astOp with
-        | Some ast -> printfn "%A" (prettyprint (evalProg ast))
-        | None -> ()
-    with 
-    | ex -> printfn "%A" "Usage: dotnet run <file.song>"
+    // try
+    let file = args.[0]
+    let input = File.ReadAllText file
+    let astOp = parse input
+    match astOp with
+    | Some ast -> printfn "%A" (prettyprint (evalProg ast))
+    | None -> ()
+
+    // let d1, d2 = readDict
+    // printfn "%A" d1["HELLO"]
+    // with 
+    // | ex -> printfn "%A" "Usage: dotnet run <file.song>"
 
     0
