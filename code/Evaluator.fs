203441c0414dab09cdc91ebca5bf799f8898a0e3
Matt Laws <mdl4@williams.edu>
2023-12-08 11:02:03 -0500

    before I break everything

--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -5,24 +5,21 @@ open Sentiments
 
 open System.Collections
 
-// type EmphAndPOS =
-// | Noun of string
-// | Adj of string
-// | Verb of string
-// | Adv of string
-// | Other of string
-
 type EmphAndPOS = {Emph: string; POS: string}
+
+// lets put everything in twice --> once with real rhyme match the other with [] rhyme match to match w things that dont need to rhyme
+// type EmphPOSAndRhyme = {emph: string; pos: string; rhyme: string list}
+// hashmaps are word -> EmphPOSAndRhyme
 
 let MATCH_POS = true
 let SEE_TRANSLATION = false
-// type wordAndPOS = {word : string; partOfSpeech: PartOfSpeech}
-type Entry = { word: string; emphAndPOS: EmphAndPOS; rhyme: string List}
+// type Entry = { word: string; emphAndPOS: EmphAndPOS; rhyme: string List}
+// type Entry = { word: string; emphPOSAndRhyme: EmphPOSAndRhyme}
+
+// type Entry = { word: string; emphAndPOS: EmphAndPOS; rhyme: string List}
 type features = { emphAndPOS: EmphAndPOS; rhyme: string List}
 
 let reuse = new Hashtable()
-
-
 
 // run checker for if words are in dictionary before running evaluator
 let readDict (preferredArr: string list) =
@@ -35,54 +32,20 @@ let readDict (preferredArr: string list) =
     let dict = File.ReadAllText "cmu_pos_dict.txt"
     let common = File.ReadAllText "common.txt"
 
-    let noun_dict = File.ReadAllText "dict_noun.txt"
-    let adj_dict = File.ReadAllText "dict_adj.txt"
-    let verb_dict = File.ReadAllText "dict_verb.txt"
-    let adv_dict = File.ReadAllText "dict_adv.txt"
-
-
 
     let dictArr = dict.Split('\n')
     let commonArr = common.Split('\n')
-    
-    let nounKey = noun_dict.Split('\n')
-    let adjKey = adj_dict.Split('\n')
-    let verbKey = verb_dict.Split('\n')
-    let advKey = adv_dict.Split('\n')
 
 
-    // let addPOStoEmph (word:string) (emph:string): EmphAndPOS = 
-    // // wordList |> List.filter(fun x -> x <> newWord)
-    //     if (MATCH_POS) then 
-    //         if (advKey |> Array.filter(fun x -> x = word.ToLower()) |> Array.length) <> 0 then 
-    //             {Emph = emph; POS = "Adv"}
-    //         else if (verbKey |> Array.filter(fun x -> x = word.ToLower()) |> Array.length) <> 0 then 
-    //             {Emph = emph; POS = "Verb"}
-    //         else if (adjKey |> Array.filter(fun x -> x = word.ToLower()) |> Array.length) <> 0 then 
-    //             {Emph = emph; POS = "Adj"}
-    //         else if (nounKey |> Array.filter(fun x -> x = word.ToLower()) |> Array.length) <> 0 then 
-    //             {Emph = emph; POS = "Noun"}
-    //         else 
-    //             {Emph = emph; POS = "Other"}
-    //     else 
-    //         {Emph = emph; POS = "Other"}
-
-    // let cinds = [|0.. (Array.length commonArr) - 1|]
-    
-    // let formatArr: Entry array = Array.create (Array.length dictArr) {word = ""; emph = ""; rhyme = [""]}
-
-    let inds = [|0.. (Array.length dictArr) - 1|]
-
-    let formatArray = inds |> Array.map (fun i -> 
-        let spl: string list = dictArr[i].Split(' ') |> Array.toList
+    let dummy = dictArr |> Array.map (fun i -> 
+        let spl: string list = i.Split(' ') |> Array.toList
         let len = (List.length spl)
         let myRhyme = if len <= 3 then [spl[len - 1]] else [spl[len - 2] ; spl[len - 1]]
         let myPOS = spl[1]
         let myEmph = spl[2..] |> List.map (fun s -> if s.Contains("2") then "2" else (if s.Contains("1") then "1" else (if s.Contains("0") then "0" else ""))) |> (String.concat "")
-        let myEmphAndPOS = {Emph = myEmph; POS = myPOS}//addPOStoEmph spl[0] myEmph
+        let myEmphAndPOS = {Emph = myEmph; POS = myPOS}
         wordToFeature.Add(spl[0], box {emphAndPOS = myEmphAndPOS; rhyme = myRhyme})
         if emphToWord.ContainsKey(myEmphAndPOS) then
-            //  (emphToWord[myEmph] = spl[0]::(emphToWord[myEmph]))
            let old: string list= emphToWord[myEmphAndPOS] |> unbox
            emphToWord.Remove(myEmphAndPOS)
            emphToWord.Add(myEmphAndPOS, box (spl[0]::old))
@@ -91,21 +54,17 @@ let readDict (preferredArr: string list) =
             emphToWord.Add(myEmphAndPOS, box [spl[0]])
         
         )
-    // for i in 0.. (Array.length dictArr) - 1 do
 
     let dummy = commonArr |> Array.map (fun i ->
         let feat: features = wordToFeature[(i.ToUpper())] |> unbox
         let myEmph = feat.emphAndPOS
         if commonEmphToWord.ContainsKey(myEmph) then
-            //  (emphToWord[myEmph] = spl[0]::(emphToWord[myEmph]))
            let old: string list = commonEmphToWord[myEmph] |> unbox
            commonEmphToWord.Remove(myEmph)
            commonEmphToWord.Add(myEmph, box (i::old))
 
         else
             commonEmphToWord.Add(myEmph, box [i])
-        
-        // commonEmphToWord.Add(i, emphToWord[i])
     
         )
 
@@ -114,7 +73,7 @@ let readDict (preferredArr: string list) =
             let feat: features = wordToFeature[(i.ToUpper())] |> unbox
             let myEmph = feat.emphAndPOS
             if preferredEmphToWord.ContainsKey(myEmph) then
-                //  (emphToWord[myEmph] = spl[0]::(emphToWord[myEmph]))
+
                 let old: string list = preferredEmphToWord[myEmph] |> unbox
                 preferredEmphToWord.Remove(myEmph)
                 preferredEmphToWord.Add(myEmph, box (i::old))
@@ -139,7 +98,6 @@ let evalKeywords kws =
 // TODO: add common words
 // TODO: basically we take 
 let convert (lines: TranslationUnit list) (wtf: Hashtable) (etw: Hashtable) (cetw: Hashtable) (petw: Hashtable)=
-    // let len = ((Array.length cmuDict) - 1)
 
     let rnd = System.Random()
     let rec helpConvert (ls: TranslationUnit list) = 
@@ -156,25 +114,20 @@ let convert (lines: TranslationUnit list) (wtf: Hashtable) (etw: Hashtable) (cet
                                 printfn "Exiting"
                                 exit(-1)
                         let myWord: features = wtf[(x.word.ToUpper())] |> unbox // used to be len
+                        printfn "%A" myWord.rhyme
                         let (myEmphAndPOS: EmphAndPOS) = myWord.emphAndPOS
-                        // printfn "%A" myWord
+
                         let translator = (
                             if petw.ContainsKey(myEmphAndPOS) then
                                 petw
                             else if cetw.ContainsKey(myEmphAndPOS) then
                                 cetw
-                            else// if etw.ContainsKey(myEmphAndPOS) then
+                            else
                                 etw
-                            // else
-                            //     printfn "Word is not contained in the CMU Dictionary"
-                            //     printfn "Exiting"
-
-                            //     exit(-1)
                             )
                         let wordList: string list = (translator[myEmphAndPOS]) |> unbox
                         let len = wordList.Length
-                        // printfn "%A" (etw.ContainsKey(myEmph))
-                        // printfn "%A" (wordList[0])
+
                         let newWord = wordList[rnd.Next() % (len)] 
 
                         let updatedWordList = wordList |> List.filter(fun x -> x <> newWord)
@@ -223,9 +176,7 @@ let evalProg (ps: Expr list) =
     (helper ps[2..])
 
 
-        
-
-let rec prettyprint (p: string list list): string=
+let rec prettyprint (p: string list list): string =
     match p with
     | [] -> ""
     | x::xs ->( x |> String.concat " ") + "\n" + (prettyprint xs)
--- code/Parser.fs
+++ code/Parser.fs
@@ -24,17 +24,10 @@ let pcletter = pletter <|> (pchar ''')
 
 let pneed a b c = (pseq a b c) <|> (pseq b a c) 
 let pword = (pmany1 pcletter) |>> stringify |>> (fun x -> if x[x.Length-3..x.Length-1] = "in'" then x[0..x.Length-4] + "ing" else x)
-
-// let pcontraction = pleft (pmany0 pletter) (pstr "in'") |>> (fun x -> (stringify x) + "ing")
-
-// let pcontraction = (pstr "in'")
-
-
-let pcorword = pword
-let pntword = pcorword |>> (fun (x) -> {translate = false; word = x})
-let ptword = pseq (pchar '!') pcorword (fun (x, y) -> {translate = true; word = y})
-let pbword = pntword <|> ptword
-let pline = pseq (pmany0 (pleft (pbword) pignore)) (pleft pbword pnl) (fun (f, e) -> Line (f @ [e]))
+let pword_no_translate = pword |>> (fun (x) -> {translate = false; word = x})
+let pword_translate = pseq (pchar '!') pword (fun (x, y) -> {translate = true; word = y})
+let pword_translation_unit = pword_no_translate <|> pword_translate
+let pline = pseq (pmany0 (pleft (pword_translation_unit) pignore)) (pleft pword_translation_unit pnl) (fun (f, e) -> Line (f @ [e]))
 
 let pkeywords = pleft ((pright (pstr "<KEYWORDS>") (pmany0 (pright pspace1 pword))) |>> (fun x -> Keywords x)) pnl
 
@@ -44,7 +37,7 @@ let pexpr = pseq (pseq psentiment pkeywords (fun (a, b) -> a::[b])) (pmany1 plin
 let grammar = pleft pexpr peof
 
 let parse s = 
-    let i = debug s
+    let i = prepare s
     match grammar i with
     | Success(ast, _) -> Some ast
     | Failure(pos, rule) -> 
